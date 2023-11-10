7c6462f23304733189e4c016e72956bcbf9d5508
shamanthhegde2 <shamanthhegde12@gmail.com>
2023-11-09 21:19:56 -0600

    largest element in an array

---
+++ Array/LargestElementInAnArray.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class LargestElementInAnArray {
+    public static void main(String[] args) {
+        int[] arr = new int[] {10, 50, 15, 40};
+        int largest = arr[0];
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        System.out.println(largest+" ");
+    }
+}
